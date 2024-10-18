cc0f06acd067f2cae37176f23e240c958ef3ff63
RWU <45728791+QGBSA@users.noreply.github.com>
2024-10-18 14:33:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-# app.py
 from flask import Flask, render_template, request
 from sentiment_analysis import analyze_sentiment
-from image_video_generation import generate_image_from_prompt
-from image_video_generation import generate_video_from_prompts
 
 app = Flask(__name__)
 
@@ -19,24 +16,5 @@ def sentiment():
         return render_template('sentiment.html', result=sentiment_result)
     return render_template('sentiment.html')
 
-# Image generation route
-@app.route('/generate_image', methods=['GET', 'POST'])
-def generate_image():
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        image_path = generate_image_from_prompt(prompt)
-        return render_template('generate_image.html', image_path=image_path)
-    return render_template('generate_image.html')
-
-# Add this route to handle video generation
-@app.route('/generate_video', methods=['GET', 'POST'])
-def generate_video():
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        video_path = generate_video_from_prompts(prompt)  # Assuming you have a function for video generation
-        return render_template('generate_video.html', video_path=video_path)
-    return render_template('generate_video.html')
-
-
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(debug=True)
